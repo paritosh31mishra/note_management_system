f8e92ed35ed45b22a1f708a2e77a0345f86aa4cb
Paritosh Mishra <paritosh31mishra@gmail.com>
2025-01-22 03:20:36 +0530

    Update login.js

--- reactapp/src/login.js
+++ reactapp/src/login.js
@@ -56,7 +56,7 @@ const Mylogin = () =>{
      <div className="container mt-5">
         <div className="row">
             <div className="col-lg-3"></div>
-            <div className="col-lg-6 w-50 border bg-secondary text-white p-5">
+            <div className="col-lg-6 w-50 border bg-dark text-white p-5">
                 <div className="border p-4 rounded shadow">
                     <h3 className="text-center"> {console.log("paritosh")} User Login </h3>
                     <div className="mb-4">
